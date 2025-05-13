74d6ab51d9ac386b05ef19f916c2622b02f7a6bf
Aleksandrs <aleksandrs.janchevskis@gmail.com>
2025-05-13 20:20:42 +0300

    Add new scenario

--- tests/lekcijas_scenariji.js
+++ tests/lekcijas_scenariji.js
@@ -15,5 +15,55 @@ module.exports = {
             .click('button[type="submit"]')
             .waitForElementVisible('span.input__error')
             .saveScreenshot('tests_screenshots/invalidEmail.png')
+    },
+
+    webRTCTests: client => {
+        const baseUrl = 'https://v3demo.mediasoup.org/?roomId=';
+        const participantName = 'part1';
+
+        const timeout = 10*1000;
+        const callDuration = 30*1000;
+
+        const selectors = {
+            participantNameField: '.display-name.editable:not(.loading)',
+            ready: '.icon.connected'
+        };
+
+        client
+            .url(baseUrl)
+            .waitForElementVisible(selectors.ready, timeout)
+
+            //Set participant's name for traceability
+            .click(selectors.participantNameField)
+            .sendKeys(selectors.participantNameField, [participantName, Keys.ENTER])
+
+            .pause(callDuration / 2)
+            .saveScreenshot('tests_screenshots/WebRTC_Call.png')
+            .pause(callDuration / 2);
+    },
+
+    webRTCTests2: client => {
+        const baseUrl = 'https://v3demo.mediasoup.org/?roomId=';
+        const participantName = 'participant 2';
+
+        const timeout = 10*1000;
+        const callDuration = 30*1000;
+
+        const selectors = {
+            participantNameField: '.display-name.editable:not(.loading)',
+            ready: '.icon.connected'
+        };
+
+        client
+            .url(baseUrl)
+            .waitForElementVisible(selectors.ready, timeout)
+
+            //Set participant's name for traceability
+            .click(selectors.participantNameField)
+            .sendKeys(selectors.participantNameField, [participantName, Keys.ENTER])
+
+            .pause(callDuration / 2)
+            .saveScreenshot('tests_screenshots/WebRTC_Call2.png')
+            .pause(callDuration / 2);
     }
 }
