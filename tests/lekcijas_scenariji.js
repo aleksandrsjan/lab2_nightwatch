d119be4ac6183ba1ea20d57662b861f55adaa614
Aleksandrs <aleksandrs.janchevskis@gmail.com>
2025-05-17 20:11:57 +0300

    Add new scenarios

--- tests/lekcijas_scenariji.js
+++ tests/lekcijas_scenariji.js
@@ -1,69 +1,287 @@
+const { assert } = require("nightwatch");
+
 module.exports = {
-    test: client => {   
-        client
-            .url('https://tdlschool.com/')
-            .maximizeWindow()
-            .waitForElementVisible('.main-home')
-            .waitForElementVisible('button.button--border')
-            .click('button.button--border')
-            .waitForElementVisible('.main-home a[href="/courses"]')
-            .click('.main-home a[href="/courses"]')
-            .waitForElementVisible('.app__page .courses')
-            .assert.urlEquals('https://tdlschool.com/courses')
-            .waitForElementVisible('input[name="email"]')
-            .sendKeys('input[name="email"]', "test.com")
-            .click('button[type="submit"]')
-            .waitForElementVisible('span.input__error')
-            .saveScreenshot('tests_screenshots/invalidEmail.png')
+    before: () => {
+        console.log('------------------------------------------------------------------------------------------');
     },
 
-    webRTCTests: client => {
-        const baseUrl = 'https://v3demo.mediasoup.org/?roomId=';
-        const participantName = 'part1';
-
-        const timeout = 10*1000;
-        const callDuration = 30*1000;
-
-        const selectors = {
-            participantNameField: '.display-name.editable:not(.loading)',
-            ready: '.icon.connected'
-        };
-
-        client
-            .url(baseUrl)
-            .waitForElementVisible(selectors.ready, timeout)
-
-            //Set participant's name for traceability
-            .click(selectors.participantNameField)
-            .sendKeys(selectors.participantNameField, [participantName, Keys.ENTER])
-
-            .pause(callDuration / 2)
-            .saveScreenshot('tests_screenshots/WebRTC_Call.png')
-            .pause(callDuration / 2);
+    beforeEach: (client) => {
+        console.log('------------------------------------------------------------------------------------------');
     },
 
-    webRTCTests2: client => {
-        const baseUrl = 'https://v3demo.mediasoup.org/?roomId=';
-        const participantName = 'participant 2';
-
-        const timeout = 10*1000;
-        const callDuration = 30*1000;
+    tdlschoolInvalidEmail: client => {
+        const data = {
+            tdlURL: 'https://tdlschool.com',
+            coursesURL: 'https://tdlschool.com/courses',
+            invalidEmailValue: 'test.com',
+            invalidEmailAddressText: 'Please provide a valid email address',
+            invalidEmailScreen: 'tests_screenshots/ivalidEmail.png'     
+        }   
 
         const selectors = {
-            participantNameField: '.display-name.editable:not(.loading)',
-            ready: '.icon.connected'
-        };
+            mainContainer: '.main-home',
+            acceptCoockiesBtn: 'button.button--border',
+            ourCoursesBtn: '.main-home a[href="/courses"]',
+            coursesMainContainer: '.app__page .courses',
+            footerEmailSubscribe: 'input[name="email"]',
+            subscribeBtn: 'button[type="submit"]',
+            invalidEmailAddress: 'span.input__error'
 
-        client
-            .url(baseUrl)
-            .waitForElementVisible(selectors.ready, timeout)
+        }
 
-            //Set participant's name for traceability
-            .click(selectors.participantNameField)
-            .sendKeys(selectors.participantNameField, [participantName, Keys.ENTER])
+        const setupPage = () =>  {
+            client
+                .url(data.tdlURL)
+                .maximizeWindow()
+                .waitForElementVisible(selectors.mainContainer)
+                .waitForElementVisible(selectors.acceptCoockiesBtn)
+                .click(selectors.acceptCoockiesBtn)
+        }
 
-            .pause(callDuration / 2)
-            .saveScreenshot('tests_screenshots/WebRTC_Call2.png')
-            .pause(callDuration / 2);
+        const ourCourses = () => {
+            client 
+                .waitForElementVisible(selectors.ourCoursesBtn)
+                .click(selectors.ourCoursesBtn)
+                .waitForElementVisible(selectors.coursesMainContainer)
+                .assert.urlEquals(data.coursesURL)
+        }
+
+        const invalidEmailSubscribeFlow = () => {
+            client 
+                .waitForElementVisible(selectors.footerEmailSubscribe)
+                .sendKeys(selectors.footerEmailSubscribe, data.invalidEmailValue)
+                .click(selectors.subscribeBtn)
+                .waitForElementVisible(selectors.invalidEmailAddress)
+                .getText(selectors.invalidEmailAddress, function(text) {
+                    client.assert.contains(text.value, data.invalidEmailAddressText);
+                })
+            client
+                .saveScreenshot(data.invalidEmailScreen);
+        }
+
+        setupPage();
+        ourCourses();
+        invalidEmailSubscribeFlow();
+    },
+
+    tdlSchoolCarousel: client => {
+        const data = {
+            tdlURL: 'https://tdlschool.com',
+            feedback: ''
+        }
+
+        const selectors = {
+            mainContainer: '.main-home',
+            acceptCoockiesBtn: 'button.button--border',
+            carouselField: '.Carousel_container__oL_eh',
+            activeTestimonials: '.slick-active.slick-current p',
+            nextCarouselBtn: '.slick-arrow.slick-next',
+            headerLogo: 'header a[href="/"]'
+        }
+
+        const openPage = () => {
+            client
+                .url(data.tdlURL)
+                .maximizeWindow()
+                .waitForElementVisible(selectors.mainContainer)
+                //jau akceptēti no iepriekšējā testa
+                // .waitForElementVisible(selectors.acceptCoockiesBtn)
+                // .click(selectors.acceptCoockiesBtn)
+        }
+
+        const switchCards = async () => {
+            const activeTextValue = await client.element(selectors.activeTestimonials).getText();
+
+            if (data.feedback === activeTextValue) {
+                throw new Error("feedback is the same as value");
+            }
+
+            data.feedback = activeTextValue;
+
+            await client.click(selectors.nextCarouselBtn).pause(3000);
+        }
+
+        const carouselFlow = async () => {
+            await client
+                .waitForElementVisible(selectors.headerLogo)
+                .click(selectors.headerLogo)
+                .waitForElementVisible(selectors.mainContainer)
+                .waitForElementVisible(selectors.activeTestimonials);
+
+            for (let i = 0; i < 3; i++) {
+                await switchCards();
+            }
+        }
+
+        openPage();
+        carouselFlow();
+    },
+
+    github: client => {
+        const data = {
+            githubURL: 'https://github.com',
+            testEmail: '1234@gmail.com',
+            testPassword: '12345',
+            alertMessage:  'Incorrect username or password.'
+        }
+
+        const selectors = {
+            mainContainer: 'main.font-mktg',
+            headerSignInBtn: '.HeaderMenu-link--sign-in',
+            loginForm: '#login',
+            loginField: '#login_field',
+            passwordField: '#password',
+            signInBtn: '.js-sign-in-button',
+            alertMessageField: '.js-flash-alert'
+        }
+        
+        const openGitHub = () => {
+            client
+                .url(data.githubURL)
+                .maximizeWindow()
+                .waitForElementVisible(selectors.mainContainer)
+        }
+
+        const openLoginPageGitHub = () => {
+            client  
+                .waitForElementVisible(selectors.headerSignInBtn)
+                .click(selectors.headerSignInBtn)
+                .waitForElementVisible(selectors.loginForm)
+        }
+
+        const incorrectLogin = () => {
+            client
+                .waitForElementVisible(selectors.loginField)
+                .sendKeys(selectors.loginField, data.testEmail)
+                .waitForElementVisible(selectors.passwordField)
+                .sendKeys(selectors.passwordField, data.testPassword)
+                .click(selectors.signInBtn)
+
+                .waitForElementVisible(selectors.alertMessageField)
+                .getText(selectors.alertMessageField, function(message) {
+                    // Nezināma iemesla dēļ dažreiz rindas beigās parādās atstarpe,
+                    // tāpēc tiek izmantots trimEnd(), lai to noņemtu.
+                    const value = message.value.trimEnd();
+                    this.assert.strictEqual(value, data.alertMessage);
+                })
+        }
+        
+        openGitHub();
+        openLoginPageGitHub();
+        incorrectLogin();
+    },
+
+    yahooInvalidEmail: client => {
+        const data = {
+            yahooURL: 'https://yahoo.com/',
+            invalidEmail: 'notEmail.com',
+            expectedInvalidEmailText:"Sorry, we don't recognize this email."
+        }
+
+        const selectors = {
+            headerContainer: '#Header',
+            acceptCoockiesBtn: 'button[value="agree"]',
+            loginContainerBtn: '#login-container',
+            loginContainer: '.login-box',
+            loginEmailField: '#login-username',
+            loginBtn: '#login-signin',
+            emailErrorField: '#username-error'
+        }
+
+        const openPage = () => {
+            client  
+                .url(data.yahooURL)
+                .maximizeWindow()
+                .waitForElementVisible(selectors.acceptCoockiesBtn)
+                .click(selectors.acceptCoockiesBtn)
+                .waitForElementVisible(selectors.headerContainer)
+        }
+
+        const invalidEmailFlow = () => {
+            client
+                .waitForElementVisible(selectors.loginContainerBtn)
+                .click(selectors.loginContainerBtn)
+                .waitForElementVisible(selectors.loginContainer)
+                .waitForElementVisible(selectors.loginEmailField)
+                .sendKeys(selectors.loginEmailField, data.invalidEmail)
+                .sendKeys(selectors.loginBtn, Keys.ENTER)
+                .waitForElementVisible(selectors.emailErrorField)
+                .getText(selectors.emailErrorField, function(invalidEmailText) {
+                    assert.strictEqual(invalidEmailText.value, data.expectedInvalidEmailText)
+                })
+        }
+        
+        openPage();
+        invalidEmailFlow();
+    },
+
+    yahooInvalidSignUp: client => {
+        const data = {
+            yahooURL: 'https://yahoo.com/',
+            firstName: 'Aleksandrs',
+            emailName: 'aleksandrs12345test',
+            password: 'Test-12345',
+            month: 'January',
+            day: '28',
+            year: '2003',
+            expectedError: 'Enter your last name.'
+        }
+
+        const selectors = {
+            headerContainer: '#Header',
+            acceptCoockiesBtn: 'button[value="agree"]',
+            loginContainerBtn: '#login-container',
+            createAccountBtn: '#createacc',
+            createAccountForm: '#account-attributes-challenge',
+            firstNameField: '#usernamereg-firstName',
+            emailField: '#usernamereg-userId',
+            passwordField: '#usernamereg-password',
+            dateMonthField: '#usernamereg-month',
+            dateDayField: '#usernamereg-day',
+            dateYearField: '#usernamereg-year',
+            checkTermsBtn: '.agree-checkbox-text',
+            submitBtn: '#reg-submit-button',
+            errorLastNameField: '#reg-error-lastName'
+        }
+
+        const openPage = () => {
+            client  
+                .url(data.yahooURL)
+                .maximizeWindow()
+                //jau akceptēti no iepriekšējā testa
+                // .waitForElementVisible(selectors.acceptCoockiesBtn)
+                // .click(selectors.acceptCoockiesBtn)
+                .waitForElementVisible(selectors.headerContainer)
+        } 
+        
+        const invalidSignupFlow = () => {
+            client
+                .waitForElementVisible(selectors.loginContainerBtn)
+                .click(selectors.loginContainerBtn)
+                .waitForElementVisible(selectors.createAccountBtn)
+                .click(selectors.createAccountBtn)
+                .waitForElementVisible(selectors.createAccountForm)
+                .waitForElementVisible(selectors.firstNameField)
+                .sendKeys(selectors.firstNameField, data.firstName)
+                .waitForElementVisible(selectors.emailField)
+                .sendKeys(selectors.emailField, data.emailName)
+                .waitForElementVisible(selectors.passwordField)
+                .sendKeys(selectors.passwordField, data.password)
+                .setValue(selectors.dateMonthField, data.month)
+                .sendKeys(selectors.dateDayField, data.day)
+                .sendKeys(selectors.dateYearField, data.year)
+                .click(selectors.checkTermsBtn)
+                .waitForElementVisible(selectors.submitBtn)
+                .click(selectors.submitBtn).pause(3000)
+            client
+                .waitForElementVisible(selectors.errorLastNameField)
+                .getText(selectors.errorLastNameField, function(ErrorText) {
+                    assert.strictEqual(ErrorText.value, data.expectedError);
+                })
+        }
+
+        openPage();
+        invalidSignupFlow();
     }
+
 }
